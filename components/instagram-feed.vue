9006bb868066b33bee50504a258153136c1110c0
Michael Tischler <e1326028@student.tuwien.ac.at>
2023-12-23 12:11:38 +0100

    setup nuxt app, add instagram component

---
+++ components/instagram-feed.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>Instagram feed</h2>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>index works</h1>
+  <InstagramFeed></InstagramFeed>
+</template>
+
+<script lang="ts" setup>
+</script>
