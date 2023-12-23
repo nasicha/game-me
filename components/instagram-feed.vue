b39cfe7c3d89ac2abafc16d16d588c22a8995947
Michael Tischler <e1326028@student.tuwien.ac.at>
2023-12-23 12:11:56 +0100

    add instagram component

--- components/instagram-feed.vue
+++ components/instagram-feed.vue
@@ -1,4 +1,9 @@
 <template>
   <h2>Instagram feed</h2>
-
+  <!-- <InstagramFeed
+    :count="12"
+    accessToken="<your token here>"
+    :pagination="true"
+    :caption="true"
+  /> -->
 </template>
