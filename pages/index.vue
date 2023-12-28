212422aa3d15a680ab7cb01d7ad4d2b548f08a81
Michael Tischler <e1326028@student.tuwien.ac.at>
2023-12-28 16:13:14 +0100

    add seo meta

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,13 @@ useHead({
   title: "Game Me · Enigma Eleganza Extravaganza",
   meta: [{ name: "description", content: "I'm different" }],
 });
-
+useSeoMeta({
+  title: "Game Me · Enigma Eleganza Extravaganza",
+  ogTitle: "Game Me · Enigma Eleganza Extravaganza",
+  ogImage: "https://images.sociablekit.com/images/game_me_2024.jpg",
+  ogUrl: "https://game-me-2024.netlify.app/",
+  ogDescription: "I'm different" ,
+});
 </script>
 
 
