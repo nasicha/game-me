4d1fdb519c7b26f29c338c03520a1f8e2f1c4c02
Michael Tischler <e1326028@student.tuwien.ac.at>
2023-12-28 16:47:06 +0100

    update og props

--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,13 @@
 <script lang="ts" setup>
 useHead({
   title: 'Game Me · Enigma Eleganza Extravaganza',
-  meta: [{ name: 'description', content: 'I\'m different' }],
-});
-useSeoMeta({
-  title: 'Game Me · Enigma Eleganza Extravaganza',
-  ogTitle: 'Game Me · Enigma Eleganza Extravaganza',
-  ogImage: 'https://images.sociablekit.com/images/game_me_2024.jpg',
-  description: 'I\'m different',
-  ogDescription: 'I\'m different',
+  meta: [
+    {name: 'description', content: 'I\'m different'},
+    { property: 'og:title', content: 'Game Me · Enigma Eleganza Extravaganza' },
+    { property: 'og:description', content: 'I\'m different' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:image', content: 'https://images.sociablekit.com/images/game_me_2024.jpg' }],
+  
 });
 </script>
 
