9ad423f02fe7b984b65876295c68a89b8fc51c83
Michael Tischler <e1326028@student.tuwien.ac.at>
2024-01-03 11:24:12 +0100

    add modal + input for password

---
+++ components/modal.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="modal-mask" >
+      <div class="modal-wrapper" @click="closeModal">
+        <div class="modal-container"  @click.stop >
+          <div class="modal-header">
+            <h3>Codewort ist korrekt!</h3>
+          </div>  
+          <div class="modal-body">
+            <p>Okay. The test is over now. You win. Click OK to watch the video. For your cake.</p>
+          </div>
+          <div class="modal-footer">
+            <button class="modal-button" @click="handleButtonClick">
+              Kuchen 🍰
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script setup lang="ts">
+const videoLink = 'https://www.youtube.com/watch?v=kmpyWk3p9GA';
+const emit = defineEmits(['close'])
+
+const handleButtonClick = () => {
+  emit('close');
+  window.open(videoLink, '_blank');
+}
+
+const closeModal = (event: MouseEvent) => {
+  console.log((event.target as HTMLElement).classList.contains('modal-wrapper'));
+  if ((event.target as HTMLElement).classList.contains('modal-wrapper')) {
+    emit('close');
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  &-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
+  }
+
+  &-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  &-container {
+    max-width: 280px;
+    margin: 0px auto;
+    padding: 1.25rem 1.75rem;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+
+  &-header {
+    margin-bottom: 1.25rem;
+  }
+
+  &-body {
+    margin-bottom: 1.25rem;
+    text-wrap: pretty;
+    line-height: 1.25rem;;
+  }
+
+  &-footer {
+    text-align: right;
+  }
+
+  &-button {
+    border: 0;
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+    background-color: rgb(67, 91, 119);
+    color: white;
+    font-size: 1rem;
+    cursor: pointer;
+    transition: all 0.2s ease;
+    &:hover {
+      background-color: rgb(52, 128, 220);
+    }
+  }
+}
+</style>
--- components/widget.vue
+++ components/widget.vue
@@ -1,7 +1,24 @@
 <template>
   <div class='sk-ww-instagram-feed-private' data-embed-id='247788'></div>
+  <div ref="passwordContainer" class="password-container">
+    <p ref="instructions" v-if="feedLoaded">Um das Codewort einzugeben, entweder dem Kanal folgen oder gleich hier ausprobieren:</p>
+    <div v-if="feedLoaded" class="input-container">
+      <input v-model="enteredPassword" placeholder="Codewort eingeben" ref="myInput" />
+      <button @click="checkPassword">></button>
+    </div>
+    <small v-if="wrongPassword" class="hint">{{'Leider falsch. Codewort in Großbuchstaben und ohne Leerzeichen schreiben, dann gibts Kuchen 🍰'}}</small>
+  </div>
+  <Transition name="modal">
+    <Modal v-if="showModal" @close="showModal = false" />
+  </Transition>
 </template>
 <script lang="ts" setup>
+const showModal = ref(false);
+const feedLoaded = ref(false);
+const pwd = 'THECAKEISALIE';
+const enteredPassword = ref('');
+const wrongPassword = ref(false);
+const passwordContainer = ref<HTMLDivElement | null>(null);
 
 onMounted(() => {
   const intervalIdButton = setInterval(() => {
@@ -18,13 +35,26 @@ onMounted(() => {
       const myDiv = document.querySelector('.sk-instagram-feed-private-bio-container');
 
       if (myDiv) {
-        myDiv.textContent = 'Iris Böhm • Meine Bewerbung als Social Media Manager in Form einer Instagram-Schnitzeljagd.'; // Replace with your desired text
+        feedLoaded.value = true;
+        myDiv.textContent = 'Iris Böhm • Meine Bewerbung als Social Media Manager in Form einer Instagram-Schnitzeljagd.';
+
+        if (passwordContainer.value) {
+          myDiv.insertAdjacentElement('afterend', passwordContainer.value);
+        }
 
         clearInterval(intervalIdProvileText);
       }
     }, 50);
 })
 
+const checkPassword = () => {
+  // Compare entered password with pwd
+  if (enteredPassword.value === pwd) {
+    showModal.value = true;
+  } else {
+    wrongPassword.value = true;
+  }
+};
 </script>
 <style lang="scss">
 .sk {
@@ -50,5 +80,56 @@ onMounted(() => {
 
 .loading-img {
   margin: 42vh 0;
+}
+
+.password-container {
+  margin-top: .5rem;
+}
+
+.input-container {
+	display: inline-flex;
+	justify-content: space-between;
+  margin-top: .5rem;
+
+
+  & > input {
+    border: 0;
+    padding: 0.3rem;
+    width: 100%;
+    font-size: .9rem;
+    transition: all .2s;
+    border: 1px solid rgb(67, 91, 119);
+    border-top-left-radius: .25rem;
+    border-bottom-left-radius: .25rem;
+    
+    &:focus {
+      outline: none;
+      border-color: rgb(52, 128, 220);
+    }
+  }
+
+  & > button {
+    border: 0;
+    width: 30px;
+    color: white;
+    background-color: rgb(67, 91, 119);
+    cursor: pointer;
+    transition: all .2s;
+    border-top-right-radius: .25rem;
+    border-bottom-right-radius: .25rem;
+    
+    &:hover {
+      background-color: rgb(52, 128, 220);
+    }
+  }
+
+}
+
+
+.hint {
+  color: red;
+  font-size: 0.8rem;
+  margin-top: 0.25rem;
+  display: block;
 }
 </style>
